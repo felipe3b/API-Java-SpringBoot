474f256d0101ebe907f01f8a395b631d83f88661
Felipe Soares <felipe3b@gmail.com>
2023-10-31 15:55:52 -0300

    Adiciono metodos para Atualizar e Deletar uma guitarra

--- src/main/java/com/ibanez/jem/jemguitar/controller/GuitarController.java
+++ src/main/java/com/ibanez/jem/jemguitar/controller/GuitarController.java
@@ -1,13 +1,21 @@
 package com.ibanez.jem.jemguitar.controller;
 
-
 import com.ibanez.jem.jemguitar.model.Guitar;
 import com.ibanez.jem.jemguitar.service.GuitarService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api")
@@ -21,33 +29,46 @@ public class GuitarController {
 
     @GetMapping("/hello")
     @ResponseStatus(HttpStatus.OK)
-    public String hello(){
+    public String hello() {
         return "Hello Guitar Player";
     }
 
     @PostMapping("/guitars")
     @ResponseStatus(HttpStatus.CREATED)
-    public Guitar criarGuitar(@RequestBody Guitar guitar){
-
-        return guitarService.criarGuitar(guitar);
+    public Guitar createGuitar(@RequestBody Guitar guitar) {
+        return guitarService.saveGuitar(guitar);
     }
 
     @GetMapping("/list")
     @ResponseStatus(HttpStatus.OK)
-    public List<Guitar> listaGuitar(){
-        return guitarService.listaGuitar();
+    public List<Guitar> listGuitars() {
+        return guitarService.getAllGuitars();
     }
 
     @GetMapping("/guitars/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public ResponseEntity<Guitar> buscaGuitarPorId(@PathVariable(value = "id") Long id){
-        return guitarService.buscaGuitarPorId(id);
+    public ResponseEntity<Object> getGuitarById(@PathVariable(value = "id") UUID id) {
+        return guitarService.getById(id);
     }
 
     @GetMapping("/guitars/search/{nome}")
     @ResponseStatus(HttpStatus.OK)
-    public List<Guitar> buscaGuitarPorNome(@PathVariable(value = "nome") String nome){
-        return guitarService.buscaGuitarPorNome(nome);
+    public List<Guitar> getGuitarByName(@PathVariable(value = "nome") String nome) {
+        return guitarService.getByName(nome);
     }
+
+    @PutMapping("/guitars/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<Object> updateGuitar(@PathVariable(value = "id") UUID id, @RequestBody Guitar guitar) {
+        return guitarService.updateGuitar(guitar, id);
+    }
+
+    @DeleteMapping("/guitars/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public ResponseEntity<Object> deleteGuitar(@PathVariable(value = "id") UUID id){
+        return guitarService.deleteGuitar(id);
+     }
+
+
 
 }
--- src/main/java/com/ibanez/jem/jemguitar/service/GuitarService.java
+++ src/main/java/com/ibanez/jem/jemguitar/service/GuitarService.java
@@ -2,10 +2,13 @@ package com.ibanez.jem.jemguitar.service;
 
 import com.ibanez.jem.jemguitar.model.Guitar;
 import com.ibanez.jem.jemguitar.repository.GuitarRepository;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class GuitarService {
@@ -17,22 +20,55 @@ public class GuitarService {
 
     private final GuitarRepository guitarRepository;
 
-    public Guitar criarGuitar(Guitar guitar) {
+    public Guitar saveGuitar(Guitar guitar) {
 
         return guitarRepository.save(guitar);
     }
 
-    public List<Guitar> listaGuitar() {
+    public List<Guitar> getAllGuitars() {
         return guitarRepository.findAll();
     }
 
-    public ResponseEntity<Guitar> buscaGuitarPorId(Long id) {
-        return guitarRepository.findById(id)
-                .map(guitar -> ResponseEntity.ok().body(guitar))
-                .orElse(ResponseEntity.notFound().build());
+    public ResponseEntity<Object> getById(UUID id) {
+        var guitarOptional = guitarRepository.findById(id);
+
+        if (guitarOptional.isPresent()) {
+            return ResponseEntity.ok(guitarOptional.get());
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Guitar not found");
+        }
     }
 
-    public List<Guitar> buscaGuitarPorNome(String nome) {
+    public List<Guitar> getByName(String nome) {
         return guitarRepository.findByNome(nome);
+    }
+
+    public ResponseEntity<Object> updateGuitar(Guitar guitar, UUID id) {
+        var guitarOptional = guitarRepository.findById(id);
+
+        if (guitarOptional.isPresent()) {
+            var guitarToUpdate = guitarOptional.get();
+            guitarToUpdate.setNome(guitar.getNome());
+            guitarToUpdate.setModelo(guitar.getModelo());
+            guitarToUpdate.setPreco(guitar.getPreco());
+            Guitar updated = guitarRepository.save(guitarToUpdate);
+
+            return ResponseEntity.ok().body(updated);
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Guitar not found");
+
+    }
+
+    public ResponseEntity<Object> deleteGuitar(UUID id) {
+        var guitarOptional = guitarRepository.findById(id);
+
+        if (guitarOptional.isPresent()) {
+            guitarRepository.delete(guitarOptional.get());
+            return ResponseEntity.status(HttpStatus.OK).body("Guitar deleted successfully.");
+
+        }
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Guitar not found");
+
     }
 }
